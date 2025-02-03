6522fef03a4cc950ebd1e7b8a458447bca3e24d1
Jerry Chen <jerry.chen1@customerservice.nsw.gov.au>
2025-02-03 13:58:06 +1100

    update packages

--- code/02-getting-started-with-react-navigation/App.js
+++ code/02-getting-started-with-react-navigation/App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import CategoriesScreen from './screens/CategoriesScreen';
-
 const Stack = createNativeStackNavigator();
 
 export default function App() {
